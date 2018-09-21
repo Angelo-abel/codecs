7b22fd6ca331341954f25c5baadfab36f1c42752
corentin <corentinalcoy@gmail.com>
2018-09-21 23:19:08 +0200

    xor_inv encoder/decoder

---
+++ encoders_decoders/xor_inv.py
@@ -0,0 +1,17 @@
+from chunker import chunker
+
+def encode(input_path: str, ouptut_filename: str)->None:
+    encode_str: bytes = b""
+    status: bool = True
+    for chunk in chunker(input_path):
+        for data in chunk:
+            if status:
+                encode_str += (data^0x65).to_bytes(1, 'little')
+            else:
+                encode_str += (data^0xFF).to_bytes(1, 'little')
+            status = not status
+    with open(ouptut_filename, 'wb') as open_file:
+        open_file.write(encode_str)
+
+def decode(input_path: str, ouptut_filename: str)->None:
+    encode(input_path, ouptut_filename)
