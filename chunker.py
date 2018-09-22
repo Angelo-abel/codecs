e87c700eaf369fa8f812c281cfe3895310d546df
corentin <corentinalcoy@gmail.com>
2018-09-22 13:56:12 +0200

    modification des codes

--- chunker.py
+++ chunker.py
@@ -2,8 +2,10 @@ def chunker(path: str):
     """Read 64 """
 # if os.isfile(path):
     with open(path, 'rb') as file_handle:
+        i: int = 0
         while True:
             data_chunk = file_handle.read(64)
+            i += 64
             if not data_chunk:
                 break
             yield data_chunk
--- encoders_decoders/xor.py
+++ encoders_decoders/xor.py
@@ -4,7 +4,7 @@ def encode(input_path: str, output_filename: str)->None:
     encode_str: bytes = b""
     for chunk in chunker(input_path):
         for data in chunk:
-            encode_str += (data^0x65).to_bytes(1, 'little')
+            encode_str += (data ^ 0x65).to_bytes(1, 'little')
     with open(output_filename, 'wb') as oepn_file:
         oepn_file.write(encode_str)
 
