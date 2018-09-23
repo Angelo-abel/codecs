5196fd5b4163690b71925001ecf021852360ffd5
corentin <corentinalcoy@gmail.com>
2018-09-23 15:23:47 +0200

    ajout de la barre de progression

--- encoders_decoders/inverter.py
+++ encoders_decoders/inverter.py
@@ -1,14 +1,22 @@
 from chunker import chunker
+from get_file_size import getFileSize
+from progressbar import progress
 
 
 def encode(input_file: str, output_file: str)->None:
+    file_size: int = getFileSize(input_file)
+    x: int  = 0
     encode_str: bytes = b""
     for chunk in chunker(input_file):
         for data in chunk:
             encode_str += (data ^ 0xFF).to_bytes(1, 'little')
+            x += 1
+            progress(x, file_size)
     with open(output_file, 'wb') as file_handle:
         file_handle.write(encode_str)
-
+    print()
+    return None
 
 def decode(input_file: str, output_file: str)->None:
     encode(input_file, output_file)
+    return None
--- encoders_decoders/xor_inv.py
+++ encoders_decoders/xor_inv.py
@@ -1,8 +1,13 @@
 from chunker import chunker
+from get_file_size import getFileSize
+from progressbar import progress
+
 
 def encode(input_path: str, ouptut_filename: str)->None:
     encode_str: bytes = b""
     status: bool = True
+    file_size: int = getFileSize(input_path)
+    x: int  = 0
     for chunk in chunker(input_path):
         for data in chunk:
             if status:
@@ -10,9 +15,14 @@ def encode(input_path: str, ouptut_filename: str)->None:
             else:
                 encode_str += (data ^ 0xFF).to_bytes(1, 'little')
             status = not status
+            x += 1
+            progress(x, file_size)
     with open(ouptut_filename, 'wb') as open_file:
         open_file.write(encode_str)
+    print()
+    return None
 
 
 def decode(input_path: str, ouptut_filename: str)->None:
     encode(input_path, ouptut_filename)
+    return None
