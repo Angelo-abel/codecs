7f15ae59941145209865533dad7a1bd851dee13e
corentin <corentinalcoy@gmail.com>
2018-09-26 10:58:30 +0200

    implement metadata into inverter codec

--- encoders_decoders/inverter.py
+++ encoders_decoders/inverter.py
@@ -1,22 +1,38 @@
 from chunker import chunker
-from get_file_size import getFileSize
+from utils import *
 from progressbar import progress
+from metadata import *
 
-
-def encode(input_file: str, output_file: str, label: str="Encoding")->None:
+def encode(input_file: str, output_file: str)->None:
     file_size: int = getFileSize(input_file)
     x: int  = 0
-    encode_str: bytes = b""
+    encode_str: bytes = metaDataGenerate() + b""
     for chunk in chunker(input_file):
         for data in chunk:
             encode_str += (data ^ 0xFF).to_bytes(1, 'little')
             x += 1
-            progress(x, file_size, label)
+            progress(x, file_size, "Encode")
     print()
-    with open(output_file, 'wb') as file_handle:
-        file_handle.write(encode_str)
+    storeFile(output_file, encode_str)
     return None
 
 def decode(input_file: str, output_file: str)->None:
-    encode(input_file, output_file, label="Decoding")
+    file_size: int = getFileSize(input_file)
+    x: int = 0
+    decode_str: bytes =  b""
+    if metaDataVerify(input_file):
+        file_size: int = getFileSize(input_file)
+        x: int = 0
+        i: int = 0
+        decode_str: bytes = b""
+        for chunk in chunker(input_file):
+            for data in chunk:
+                if i > 64:
+                    decode_str += (data ^ 0xFF).to_bytes(1, 'little')
+                else:
+                    i += 1
+                x += 1
+                progress(x, file_size, 'Decode')
+        print()
+        storeFile(output_file, decode_str)
     return None
